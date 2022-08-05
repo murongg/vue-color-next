efd0b7263ac9b31e2cd77cb62e7662cb83242d86
木荣 <372638156@qq.com>
2022-08-06 00:48:18 +0800

    feat: automatically generate version

---
+++ scripts/genrate-version.ts
@@ -0,0 +1,11 @@
+import { writeFileSync } from 'fs'
+import path from 'path'
+
+function generateVersion() {
+  const basePath = path.join(__dirname, '../src/version.ts')
+  // eslint-disable-next-line @typescript-eslint/no-var-requires
+  const version = require('../package.json').version
+  writeFileSync(basePath, `export default '${version}'`)
+}
+
+generateVersion()
--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,6 @@
 import { makeInstaller } from './helpers/install'
 import components from './components'
-
+import version from './version'
 const VueColor = makeInstaller([...components])
 
 const install = VueColor.install
@@ -8,6 +8,7 @@ const install = VueColor.install
 export * from './types'
 
 export {
+  version,
   components,
   install,
   VueColor,
