b1375c95bda7b8d98b8229cca77a5e58e368f05a
木荣 <372638156@qq.com>
2022-08-07 10:04:55 +0800

    chore: update example

--- examples/src/App.vue
+++ examples/src/App.vue
@@ -1,5 +1,29 @@
+<script setup lang="ts">
+import type { StyleValue } from 'vue'
+import type { ModelValue } from '../../src/types'
+const colors = ref<ModelValue>('#84ACDAFF')
+
+const style = computed<StyleValue>(() => {
+  const color = typeof colors.value === 'string' ? colors.value : colors.value.hex
+  return {
+    background: color,
+  }
+})
+
+</script>
+
 <template>
-  <main font-sans p="x-4 y-10" text="center gray-700 dark:gray-200">
-    <router-view />
+  <main font-sans w-screen h-screen p="x-4 y-10" text="white dark:gray-200" :style="style">
+    <a href="https://github.com/murongg/vue-color-next" target="_blank" absolute text-4xl right-5 top-5 icon-btn i-carbon-logo-github />
+    <div flex>
+      <div flex flex-col mr-5>
+        <sketch v-model="colors" />
+        <span text-center mt-1>sketch</span>
+      </div>
+      <div flex flex-col mr-5>
+        <chrome v-model="colors" />
+        <span text-center mt-1>chrome</span>
+      </div>
+    </div>
   </main>
 </template>
--- examples/src/main.ts
+++ examples/src/main.ts
@@ -1,6 +1,4 @@
 import { createApp } from 'vue'
-import { createRouter, createWebHistory } from 'vue-router'
-import routes from 'virtual:generated-pages'
 import { VueColor } from '../../src'
 import App from './App.vue'
 
@@ -12,10 +10,5 @@ import '../../src/styles/sketch.css'
 import '../../src/styles/chrome.css'
 
 const app = createApp(App)
-const router = createRouter({
-  history: createWebHistory(import.meta.env.BASE_URL),
-  routes,
-})
-app.use(router)
 app.use(VueColor)
 app.mount('#app')
